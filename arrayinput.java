9879b466bff75a79db506b66067777b6ba0653a9
Roshan <mishraroshan1101@gmail.com>
2024-03-23 02:25:33 +0530

    new changes

--- arrayinput.java
+++ arrayinput.java
@@ -1,5 +1,16 @@
+import java.util.*;
 public class arrayinput {
     public static void main(String[] args){
-        System.out.println("hello");
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        int array[] = new int[n];
+        for(int i =0;i <n;i++){
+            array[i] = input.nextInt();
+        }
+        input.close();
+        for(int j =0;j<array.length;j++){
+            System.out.println(array[j]);
+        }
     }
 }
