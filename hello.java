b5e57458caca7d4e0c6d5fe9f94d57e58a46101a
Roshan <mishraroshan1101@gmail.com>
2024-03-23 01:30:39 +0530

    first commit

---
+++ hello.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class hello{
+ 
+
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        int n = input.nextInt();
+        int [] array = new int[n];
+    }
+
+
+}
